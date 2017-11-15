cd58e5713c3453f5da15fd2f744c3b7a1d496833
Иван Турьев <onlyfl@ukr.net>
2017-11-15 07:52:45 +0200

    15/11

---
+++ js/imperio.js
@@ -0,0 +1,31 @@
+	
+		 				
+
+	
+$(document).on('click','.full_form', function() {
+		   $('.full_form').addClass('active');
+		    $('.short_form').removeClass('active');
+			 $('.form_block_full').fadeIn('1000');
+
+    });
+	
+	$(document).on('click','.short_form', function() {
+		   $('.short_form').addClass('active');
+		    $('.full_form').removeClass('active');
+			 $('.form_block_full').fadeOut('1000');
+
+    });
+	
+	
+	
+	
+	
+
+
+
+	
+	
+	
+
+	
+	
